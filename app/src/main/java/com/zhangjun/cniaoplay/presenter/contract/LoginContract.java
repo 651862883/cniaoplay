2ed474d694c36d68322c47f81846cb244694e324
zhangjun <651862883@qq.com>
2017-11-15 14:22:49 +0800

    11

---
+++ app/src/main/java/com/zhangjun/cniaoplay/presenter/contract/LoginContract.java
@@ -0,0 +1,26 @@
+package com.zhangjun.cniaoplay.presenter.contract;
+
+import com.zhangjun.cniaoplay.bean.BaseBean;
+import com.zhangjun.cniaoplay.bean.LoginBean;
+import com.zhangjun.cniaoplay.ui.BaseView;
+
+import rx.Observable;
+
+/**
+ * Created by Administrator on 2017-11-13.
+ */
+public interface LoginContract {
+
+   public  interface  ILoginModel{
+        public Observable<BaseBean<LoginBean>> login(String phone, String pwd);
+   }
+
+    public  interface LoginView extends BaseView{
+        public void checkPhoneError();
+        void  checkPhoneSuccess();
+
+        void  loginSuccess(LoginBean bean);
+       // void  loginError(String msg); 错误toast弹出，没有业务操作
+    }
+
+}
